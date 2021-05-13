9bcf2e8e614f7d8c063c0625f89f50cd6a49af40
cedricbahirwe <cedricaganzebahirwe@gmail.com>
2021-05-13 23:54:34 +0200

    Add New UI

---
+++ Dialer/Dialer/Views/NewDialingView.swift
@@ -0,0 +1,20 @@
+//
+//  NewDialingView.swift
+//  Dialer
+//
+//  Created by Cédric Bahirwe on 13/05/2021.
+//
+
+import SwiftUI
+
+struct NewDialingView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct NewDialingView_Previews: PreviewProvider {
+    static var previews: some View {
+        NewDialingView()
+    }
+}
