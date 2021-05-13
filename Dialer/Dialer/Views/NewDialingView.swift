35461cb7194e54e23a6d26d11884cf2c4edd5878
cedricbahirwe <cedricaganzebahirwe@gmail.com>
2021-05-14 01:05:52 +0200

    Implement Quick incognito mode

--- Dialer/Dialer/Views/NewDialingView.swift
+++ Dialer/Dialer/Views/NewDialingView.swift
@@ -9,33 +9,91 @@ import SwiftUI
 
 struct NewDialingView: View {
     @State private var composedCode: String = ""
+    @State private var showInValidMsg: Bool = false
     var body: some View {
         NavigationView {
-            VStack(spacing: 30) {
-                Spacer()
-
-                if !composedCode.isEmpty {
-                    LinearGradient(gradient: Gradient(colors: [Color.red, Color.blue]), startPoint: .leading, endPoint: .trailing)
-                        .frame(maxWidth: .infinity)
-                        .frame(height: 30)
-                        .mask(Text(composedCode))
-                        .font(Font.title.bold())
-                }
-                PinView(input: $composedCode.animation(), btnSize: .init(width: 80, height: 80))
-                    .font(Font.title2.bold())
-                Button(action: {
+            VStack(spacing: 10) {
+                Group {
                     
+                    Text("Invalid code. Check it and try again.")
+                        .font(.system(size: 16))
+                        .foregroundColor(.red)
+                        .multilineTextAlignment(.center)
+                        .opacity(showInValidMsg ? 1 : 0)
+                    LinearGradient(gradient: Gradient(colors: [Color.red, Color.blue]), startPoint: .leading, endPoint: .trailing)
+                        .frame(height: 28)
+                        .mask(Text(composedCode))
+                        .font(Font.title)
+                        .lineLimit(1)
+                        .minimumScaleFactor(0.8)
+                        .padding(.horizontal, 20)
+                        .truncationMode(.head)
+                        .opacity(composedCode.isEmpty ? 0 : 1)
+                }
+                
+                PinView(input: $composedCode.animation(), fullMode: true, btnSize: .init(width: 80, height: 80))
+                    .font(Font.title.bold())
+                    .padding(.vertical, 10)
+
+                Button(action: {
+                    dial(composedCode)
                 }, label: {
                     Image(systemName: "phone.circle.fill")
                         .resizable()
                         .scaledToFit()
-                        .frame(width: 80, height: 80)
+                        .frame(width: 78, height: 78)
                         .clipShape(Circle())
                         .foregroundColor(.primary)
                 })
+                .frame(maxWidth: .infinity)
+                .overlay(
+                    Button(action: {
+                        if !composedCode.isEmpty {
+                            composedCode.removeLast()
+                        }
+                    }, label: {
+                        Image(systemName: "delete.left.fill")
+                            .resizable()
+                            .aspectRatio(contentMode: .fit)
+                            .frame(width: 40, height: 25, alignment: .trailing)
+                            .foregroundColor(Color.red.opacity(0.8))
+                    })
+                    .padding(30)
+                    .opacity(composedCode.isEmpty ? 0 : 1)
+                    ,alignment: .trailing
+                )
             }
-            .padding(.bottom, 20)
+            .padding(.vertical, 15)
             .navigationTitle("Incognito Mode👨🏽‍💻𒆂")
+        }
+    }
+    
+    private func dial(_ code: String) {
+        // Basic Checks
+        // This can be removed when user wants to dial a phone number ....
+        if code.contains("*") && code.contains("#") && code.count >= 5 {
+            if let telUrl = URL(string: "tel://\(code)"), UIApplication.shared.canOpenURL(telUrl) {
+                UIApplication.shared.open(telUrl, options: [:], completionHandler: { _ in
+                    print("Finishinga")
+                    
+                })
+//                UIApplication.shared.endEditing(true)
+
+            } else {
+                // Can not dial this code
+                manageInvalidCode()
+            }
+            
+        } else {
+            // Supposed to be invalid, Can not dial this code
+            manageInvalidCode()
+        }
+    }
+    
+    private func manageInvalidCode() {
+        showInValidMsg = true
+        DispatchQueue.main.asyncAfter(deadline: .now()+2) {
+            showInValidMsg = false
         }
     }
 }
