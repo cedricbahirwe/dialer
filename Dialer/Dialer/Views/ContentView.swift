ae3c6ef670245ae12f000fb7240c8f9b220c6122
cedricbahirwe <cedricaganzebahirwe@gmail.com>
2021-09-06 20:15:29 +0200

    Add Staging

--- Dialer/Dialer/Views/ContentView.swift
+++ Dialer/Dialer/Views/ContentView.swift
@@ -8,6 +8,12 @@
 import SwiftUI
 import Foundation
 
+
+//if let currentVersion = Bundle.main.infoDictionary?["CFBundleVersion"] as? String {
+//    print(currentVersion)
+//}
+
+
 struct ContentView: View {
     @EnvironmentObject private var data: MainViewModel
     var body: some View {
