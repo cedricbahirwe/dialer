f1996b86a4e70a88507117cca1d7193f92565be1
Cedric Bahirwe <cedricaganzebahirwe@gmail.com>
2022-02-17 00:43:38 +0200

    Fix Strings

--- Dialer/Dialer/Views/SubViews/PinView.swift
+++ Dialer/Dialer/Views/SubViews/PinView.swift
@@ -8,12 +8,13 @@
 import SwiftUI
 
 struct PinView: View {
+    @State private var didCopyToClipBoard = false
     @Binding var input: String
     public var isFullMode: Bool = false
     public var btnSize: CGFloat = 60
 
     private var buttons: [String] {
-        var defaults: [String] = ["1","2","3","4","5","6","7","8","9","*","0"]
+        var defaults: [String] = ["1","2","3","4","5","6","7","8", "9", isFullMode ? "*" : "copy","0"]
         
         defaults += isFullMode ? ["#"] : ["X"]
         return defaults
@@ -26,27 +27,51 @@ struct PinView: View {
             GridItem(.flexible()),
         ], spacing: 10) {
             ForEach(buttons, id: \.self) { button in
-                CircleButton(button, size: btnSize,
-                             action: { addKey(button)})
-                .foregroundColor(
-                    button == "X" ?
-                        Color.red :
-                        Color(.label)
-                )
-                .opacity(!isFullMode && button == "*" ? 0 : 1)
-                .opacity(input.isEmpty && button == "X" ? 0 : 1)
+                ZStack {
+                    CircleButton(button, size: btnSize,
+                                 action: { addKey(button)})
+                        .foregroundColor(
+                            button == "X" ?
+                            Color.red :
+                                Color(.label)
+                        )
+                        .opacity(!isFullMode && button == "*" ? 0 : 1)
+                        .opacity(input.isEmpty && button == "X" ? 0 : 1)
+                        .opacity((button == "copy" && didCopyToClipBoard) ? 0 : 1)
+
+                    if (button == "copy" && didCopyToClipBoard) {
+                        Image(systemName: "checkmark")
+                            .font(.callout.weight(.black))
+                            .foregroundColor(.green)
+                            .frame(width: btnSize, height: btnSize)
+                            .background(Color.gray.opacity(0.2))
+                            .clipShape(Circle())
+                    }
+                }
                 
             }
         }
     }
     
     private func addKey(_ value: String) {
+        if value == "copy" {
+            copyToClipBoard()
+            return
+        }
         if value == "X" {
             if !input.isEmpty {
                 input.removeLast()
             }
         } else {
             input.append(value)
+        }
+    }
+
+    private func copyToClipBoard() {
+        UIPasteboard.general.string = input
+        didCopyToClipBoard = true
+        DispatchQueue.main.asyncAfter(deadline: .now()+2) {
+            didCopyToClipBoard = false
         }
     }
 }
@@ -80,10 +105,17 @@ struct CircleButton: View {
     
     var body: some View {
         Button(action: action) {
-            Text(title)
-                .frame(width: size, height: size)
-                .background(Color.gray.opacity(0.2))
-                .clipShape(Circle())
+            Group {
+                if title == "copy" {
+                    Image(systemName: "doc.on.doc.fill")
+                        .frame(width: 45, height: 45)
+                } else {
+                    Text(title)
+                }
+            }
+            .frame(width: size, height: size)
+            .background(Color.gray.opacity(0.2))
+            .clipShape(Circle())
         }
     }
 }
