c4c08a045f69ece7a0027bfa115469e6e4b44a23
Cedric Bahirwe <cedricaganzebahirwe@gmail.com>
2022-12-11 13:19:13 +0200

    Remove Test Data

--- Dialer/Dialer/AppDelegate.swift
+++ Dialer/Dialer/AppDelegate.swift
@@ -7,18 +7,10 @@
 
 import UIKit
 
-#if DEV
-    let SOME_SERVICE_KEY = "SomeKeyForDEV"
-#else
-    let SOME_SERVICE_KEY = "SomeKeyForPRO"
-#endif
-
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
     
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-        print(SOME_SERVICE_KEY)
         return true
     }
 }
-
