5a35aaf83de7c2fc01244165bccf9159312200e2
Cedric Bahirwe <cedricaganzebahirwe@gmail.com>
2023-03-01 11:35:36 +0200

    Refactor to add more decoupling

--- Dialer/Dialer/Utilities/FirebaseManager.swift
+++ Dialer/Dialer/Utilities/FirebaseManager.swift
@@ -6,27 +6,99 @@
 //
 
 import Foundation
+import FirebaseFirestore
 
 protocol MerchantProtocol {
-    func createMerchant(_ merchant: Merchant)
+    func createMerchant(_ merchant: Merchant) async -> Bool
     func getMerchant(by id: UUID) -> Merchant?
-    func getAllMerchants() -> [Merchant]
+    func deleteMerchant(_ merchantID: String) async throws
+    func getAllMerchants() async -> [Merchant]
     func getMerchantsNear(lat: Double, long: Double) -> [Merchant]
 }
 
 final class FirebaseManager: MerchantProtocol {
+    private lazy var db = Firestore.firestore()
 
-    func createMerchant(_ merchant: Merchant) {}
+    func createMerchant(_ merchant: Merchant) async -> Bool {
+        do {
+
+            return try await withCheckedThrowingContinuation { (continuation: CheckedContinuation<Bool, Error>) in
+                do {
+                    _ = try db.collection(Collection.merchants).addDocument(from: merchant) { error in
+
+                        if let error {
+                            continuation.resume(throwing: error)
+                        } else {
+                            continuation.resume(returning: true)
+                        }
+                    }
+                } catch {
+                    debugPrint("Could not save merchant: \(error.localizedDescription).")
+                    continuation.resume(throwing: error)
+                }
+            }
+        } catch {
+            debugPrint("Could not save merchant: \(error.localizedDescription).")
+            return false
+        }
+    }
 
     func getMerchant(by id: UUID) -> Merchant? {
         nil
     }
 
-    func getAllMerchants() -> [Merchant] {
-        return []
+    func deleteMerchant(_ merchantID: String) async throws {
+        try await db.collection(Collection.merchants)
+            .document(merchantID)
+            .delete()
+    }
+
+    func getAllMerchants() async -> [Merchant] {
+        do {
+            return try await withCheckedThrowingContinuation { (continuation: CheckedContinuation<[Merchant], Error>) in
+                db.collection(Collection.merchants)
+                    .addSnapshotListener { querySnapshot, error in
+                        if let error = error {
+                            continuation.resume(throwing: error)
+                            return
+                        }
+
+                        if let querySnapshot = querySnapshot {
+                            let result = querySnapshot.documents.compactMap { document -> Merchant? in
+                                do {
+                                    return try document.data(as: Merchant.self)
+                                } catch {
+                                    debugPrint("Firestore Decoding error: ", error, querySnapshot.documents.forEach { print($0.data()) } )
+                                    return nil
+                                }
+                            }
+
+                            continuation.resume(returning: result)
+                        }
+                    }
+            }
+        } catch {
+            debugPrint("Firestore Merchant Error: \(error).")
+            return []
+        }
     }
 
     func getMerchantsNear(lat: Double, long: Double) -> [Merchant] {
         return []
     }
+
+
+    enum Collection {
+        static let merchants = "merchants"
+    }
 }
+
+// MARK: - Merchant Side
+//extension MerchantStore {
+//    func getNearbyMerchants() async -> [Merchant] {
+//        guard let location = getLastKnownLocation() else { return [] }
+//        return merchantProvider.getMerchantsNear(
+//            lat: location.latitude,
+//            long: location.longitude)
+//    }
+//}
--- Dialer/Dialer/View Models/MerchantViewModel.swift
+++ Dialer/Dialer/View Models/MerchantViewModel.swift
@@ -5,45 +5,31 @@
 //  Created by Cédric Bahirwe on 28/02/2023.
 //
 
-import FirebaseFirestore
 import CoreLocation
 
 final class MerchantStore: ObservableObject {
     @Published private(set) var merchants: [Merchant] = []
     @Published private(set) var isFetching = false
+    private let merchantProvider: MerchantProtocol
 
-    private lazy var db = Firestore.firestore()
-
-
-    init() {
-        getAllMerchants()
+    init(_ merchantProvider: MerchantProtocol = FirebaseManager()) {
+        self.merchantProvider = merchantProvider
+        Task {
+            await getAllMerchants()
+        }
     }
 
+
+    /// Save a new Merchant
+    /// - Parameter merchant: new merchant to be saved
+    /// - Returns: Whether or not the merchant was saved
     func saveMerchant(_ merchant: Merchant) async -> Bool {
-        do {
-            isFetching = true
-            
-            return try await withCheckedThrowingContinuation { (continuation: CheckedContinuation<Bool, Error>) in
-                do {
-                    _ = try db.collection(Collection.merchants).addDocument(from: merchant) { error in
-                        DispatchQueue.main.async {
-                            self.isFetching = false
-                        }
-                        if let error {
-                            continuation.resume(throwing: error)
-                        } else {
-                            continuation.resume(returning: true)
-                        }
-                    }
-                } catch {
-                    debugPrint("Could not save merchant: \(error.localizedDescription).")
-                    continuation.resume(throwing: error)
-                }
-            }
-        } catch {
-            debugPrint("Could not save merchant: \(error.localizedDescription).")
-            return false
-        }
+        isFetching = true
+        let isMerchantSaved = await merchantProvider.createMerchant(merchant)
+
+        isFetching = false
+
+        return isMerchantSaved
     }
 
     func deleteMerchants(at offsets: IndexSet) {
@@ -55,6 +41,10 @@ final class MerchantStore: ObservableObject {
         }
     }
 
+    /// Delete a specific Merchant
+    /// - Parameters:
+    ///   - merchant: merchant to be deleted
+    ///   - index: the index of the merchant element in the collection
     private func deleteMerchant(_ merchant: Merchant, at index: IndexSet.Element) async {
         guard let merchantID = merchant.id else { return }
         
@@ -62,10 +52,9 @@ final class MerchantStore: ObservableObject {
             self.isFetching = true
             self.merchants.remove(at: index)
         }
+
         do {
-            try await db.collection(Collection.merchants)
-                .document(merchantID)
-                .delete()
+            try await merchantProvider.deleteMerchant(merchantID)
             DispatchQueue.main.async {
                 self.isFetching = false
             }
@@ -77,50 +66,31 @@ final class MerchantStore: ObservableObject {
         }
     }
 
-    func getAllMerchants() {
+
+    /// Get All Merchants Available
+    func getAllMerchants() async {
         isFetching = true
-        db.collection(Collection.merchants)
-            .addSnapshotListener { querySnapshot, error in
-                if let error = error {
-                    debugPrint("Firestore error: \(error).")
-                    return
-                }
 
-                DispatchQueue.main.async {
-                    self.isFetching = false
-                }
+        let result = await merchantProvider.getAllMerchants()
 
-                if let querySnapshot = querySnapshot {
-                    let result = querySnapshot.documents.compactMap { document -> Merchant? in
-                        do {
-                            let res = try document.data(as: Merchant.self)
-                            debugPrint("Complete", res)
-                            return res
-                        } catch {
-                            debugPrint("Firestore Decoding error: ", error, querySnapshot.documents.forEach { print($0.data()) } )
-                            return nil
-                        }
-                    }
-
-                    DispatchQueue.main.async {
-                        print(result )
-                        self.merchants = result
-                    }
-                }
-            }
+        DispatchQueue.main.async {
+            self.isFetching = false
+            self.merchants = result
+        }
     }
 
+    /// Get Filtered merchants near user's location
+    /// - Parameter userLocation: the current user's location
+    /// - Returns: Merchants sorted by their distance to the user
     func getNearbyMerchants(_ userLocation: UserLocation) -> [Merchant] {
-        let userLocation = CLLocation(latitude: userLocation.latitude, longitude: userLocation.longitude)
+        let userLocation = CLLocation(latitude: userLocation.latitude,
+                                      longitude: userLocation.longitude)
+
         let sortedMerchants = merchants.sorted {
             let location1 = CLLocation(latitude: $0.location.latitude, longitude: $0.location.longitude)
             let location2 = CLLocation(latitude: $1.location.latitude, longitude: $1.location.longitude)
             return userLocation.distance(from: location1) < userLocation.distance(from: location2)
         }
         return sortedMerchants
-    }
-
-    enum Collection {
-        static let merchants = "merchants"
     }
 }
