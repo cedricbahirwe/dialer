bb2477fa21dbe3a6982b066c5682bc7f48bda8a7
Cedric Bahirwe <cedricaganzebahirwe@gmail.com>
2023-03-27 13:58:26 +0200

    Fix Typo

--- Dialer/Dialer/Models/PurchaseDetailModel.swift
+++ Dialer/Dialer/Models/PurchaseDetailModel.swift
@@ -8,7 +8,7 @@
 import Foundation
 
 struct PurchaseDetailModel: Hashable, Codable {
-    private var prefixCode: String { "*18*2*2*1*1*1*" }
+    private var prefixCode: String { "*182*2*1*1*1*" }
     var amount: Int = 0
     var type: CodeType = .momo
     var fullCode: String {
@@ -16,7 +16,8 @@ struct PurchaseDetailModel: Hashable, Codable {
     }
     
     func getDialCode(pin: String) -> String {
-        if pin.isEmpty {
+        // 27/03/2023: MTN disabled the ability to dial airtime USSD that includes Momo PIN for an amount greater than 100.
+        if amount > 100 || pin.isEmpty {
             return "\(prefixCode)\(amount)#"
         } else {
             return "\(prefixCode)\(amount)*\(pin)#"
