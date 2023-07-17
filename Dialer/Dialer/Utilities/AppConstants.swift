49a75a694f10fe612a66b4e7075aa3e9b754642d
nshutinicolas <nshuti.nicolas@yahoo.com>
2023-07-17 11:54:56 +0200

    Made PIN entry optional, enable appending when pin available for the range of 10..<100

---
+++ Dialer/Dialer/Utilities/AppConstants.swift
@@ -0,0 +1,14 @@
+//
+//  AppConstants.swift
+//  Dialer
+//
+//  Created by Nicolas Nshuti on 17/07/2023.
+//
+
+import Foundation
+
+struct AppConstants {
+    // Airtime USSD Dial constants
+    static let minAmount = 1
+    static let allowedAmountRangeForPin = 10..<100
+}
