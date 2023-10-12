93141d1e7875e9a61d00ac3fe568001931dfa4d1
Cedric Bahirwe <cedricaganzebahirwe@gmail.com>
2023-10-12 20:57:53 +0200

    Remove test comment

--- Dialer/Dialer/AppDelegate.swift
+++ Dialer/Dialer/AppDelegate.swift
@@ -13,7 +13,6 @@ import RevenueCat
 class AppDelegate: UIResponder, UIApplicationDelegate {
     
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-        Thread.sleep(forTimeInterval: 3.0)
         DialerStorage.shared.saveOneTimeUniqueAppID()
         configureFirebase()
         configureRevenuCat()
