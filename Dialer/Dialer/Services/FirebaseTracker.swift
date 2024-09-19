e59608d5dce59923b27e50977192e90e319d96f5
Cedric Bahirwe <cedricaganzebahirwe@gmail.com>
2024-09-19 22:31:27 +0200

    Complete analytics fix

--- Dialer/Dialer/Services/FirebaseTracker.swift
+++ Dialer/Dialer/Services/FirebaseTracker.swift
@@ -45,11 +45,6 @@ class FirebaseTracker {
     func logAnalyticsEvent(_ eventName: String, parameters: [String: Any]?) {
         var completeParameters: [String : Any] = parameters ?? [:]
         completeParameters[AnalyticsParameterExtendSession] = true
-        for element in completeParameters.values {
-            if element is UUID {
-                print("The envent is", eventName)
-            }
-        }
         Analytics.logEvent(
             eventName,
             parameters: completeParameters
