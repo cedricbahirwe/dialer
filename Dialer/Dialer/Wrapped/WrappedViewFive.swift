0b66907f53dddf87e138baa2dfccb4bb4a46f4f5
Cédric Bahirwe <cedricaganzebahirwe@gmail.com>
2024-12-29 14:27:55 +0200

    Update Wrapped five

--- Dialer/Dialer/Wrapped/WrappedViewFive.swift
+++ Dialer/Dialer/Wrapped/WrappedViewFive.swift
@@ -8,116 +8,86 @@
 
 import SwiftUI
 
+let deval = false
+
 struct WrappedViewFive: View {
-    @State private var showText = false
-    @State private var showList = false
-    @State private var animatedBackground = false
-    @State private var navigateToNextPage = false
+    @State private var showText = deval
+    @State private var showList = deval
+    @State private var showdelay = false
+
     var body: some View {
+        ZStack {
+            AnimatedBackground()
 
-        ScrollView {
-            ZStack {
-                // Background Animation
-                AnimatedBackground()
-                    .ignoresSafeArea()
+            VStack(alignment: .leading) {
+                if showText {
+                    Text("Your spendings")
+                        .font(.largeTitle)
+                        .fontWeight(.bold)
+                        .multilineTextAlignment(.center)
+                        .cornerRadius(10)
+                        .transition(.scale)
+                        .animation(.easeIn(duration: 1), value: showText)
+                        .padding(.horizontal)
+                }
 
-//                VStack {
-//                    Spacer()
-//
-//                    // Main Text with Animation
-//                    if showText {
-//                        Text("Your spendings")
-//                            .font(.largeTitle)
-//                            .fontWeight(.bold)
-//                            .foregroundColor(.black)
-//                            .multilineTextAlignment(.center)
-//                            .padding(.top,25)
-//                        //                        .background(Color.yellow.opacity(0.7))
-//                            .cornerRadius(10)
-//                            .transition(.scale)
-//                            .animation(.easeIn(duration: 1), value: showText)
-//                    }
-//
-//
-//
-//                    // Top Songs List
-//                    if showList {
-//                        VStack(alignment: .leading, spacing: 10) {
-//                            //                        Text("Your Top Songs 2024")
-//                            //                            .font(.title)
-//                            //                            .fontWeight(.bold)
-//                            //                            .foregroundColor(.black)
-//
-//                            ForEach(topSongs.indices, id: \.self) { index in
-//                                HStack {
-//                                    Text("\(index + 1)")
-//                                        .font(.title3)
-//                                        .fontWeight(.bold)
-//                                        .foregroundColor(.black)
-//
-//                                    VStack(alignment: .leading) {
-//                                        Text(topSongs[index].title)
-//                                            .font(.headline)
-//                                            .foregroundColor(.black)
-//                                        Text(topSongs[index].artist)
-//                                            .font(.subheadline)
-//                                            .foregroundColor(.gray)
-//                                    }
-//                                    Spacer()
-//                                    Image(topSongs[index].imgess) // Replace with your image
-//                                        .resizable()
-//                                        .scaledToFit()
-//                                        .frame(width: 80, height: 80)
-//                                        .cornerRadius(8)
-//                                        .shadow(radius: 10)
-//                                }
-//
-//                                .padding()
-//                                .background(Color.yellow.opacity(1.5))
-//                                .cornerRadius(20)
-//                                .transition(.slide)
-//                                .animation(.easeInOut(duration: 1.2), value: showList)
-//                            }
-//                        }  .padding()
-//                            .background(Color.white)
-//                            .cornerRadius(8)
-//
-//                    }
-//
-//                    Spacer()
-//
-//                    // Share Button
-//                    Button(action: {
-//                        print("Share your Wrapped story")
-//                    }) {
-//                        Text("Share Your Story")
-//                            .fontWeight(.bold)
-//                            .padding()
-//                            .frame(maxWidth: .infinity)
-//                            .background(Color.black)
-//                            .foregroundColor(.white)
-//                            .cornerRadius(8)
-//                    }
-//                    .padding()
-//
-//                    //                    NavigationLink(destination: WrappedViewSix(), isActive: $navigateToNextPage) {
-//                    //                                      EmptyView()
-//                    //                                  }
-//                }
+                // Top Songs List
+                if showList {
+                    VStack(alignment: .leading, spacing: 25) {
+                        ForEach(spendings.indices, id: \.self) { index in
+                            HStack(spacing: 16) {
+
+                                Text("\(index + 1)")
+                                    .font(.system(.largeTitle, design: .monospaced, weight: .black))
+
+                                Divider()
+                                    .frame(maxHeight: 100)
+
+                                VStack(alignment: .leading) {
+                                    Text(spendings[index].title)
+                                        .font(.title2.bold())
+
+                                    Text(spendings[index].amount.formatted(.currency(code: "RWF")))
+                                        .font(.subheadline)
+                                        .foregroundColor(.gray)
+                                }
+
+                                Spacer()
+
+                                Text(spendings[index].percentage.formatted(.percent.precision(.fractionLength(1))))
+                                    .font(.system(.title, design: .serif, weight: .heavy))
+                            }
+                            .padding()
+                            .background(.thinMaterial, in: .rect(cornerRadius: 20))
+                            .shadow(color: .lightShadow, radius: 10)
+                            .transition(.move(edge: .leading))
+                            .offset(y: !showdelay ? -CGFloat(index) * 100 : 0)
+//                            .transition(.asymmetric(insertion: .slide, removal: .scale))
+//                            .animation(.easeInOut(duration: 1.2), value: showList)
+//                            .opacity(showList ? 1 : 0)
+                        }
+                    }
+                    .padding()
+                }
             }
-            .task {
-                DispatchQueue.main.asyncAfter(deadline: .now() + 8) {
-                    navigateToNextPage = true
+        }
+        .frame(maxWidth: .infinity)
+        .foregroundStyle(.white)
+        .task {
+            DispatchQueue.main.asyncAfter(deadline: .now() + 1) {
+                withAnimation {
+                    showText = true
                 }
-                DispatchQueue.main.asyncAfter(deadline: .now() + 1) {
-                    withAnimation {
-                        showText = true
-                    }
+            }
+            DispatchQueue.main.asyncAfter(deadline: .now() + 2.25) {
+                withAnimation(.spring()) {
+                    showList = true
                 }
-                DispatchQueue.main.asyncAfter(deadline: .now() + 2.25) {
-                    withAnimation {
-                        showList = true
-                    }
+            }
+
+            DispatchQueue.main.asyncAfter(deadline: .now() + 2.75) {
+                withAnimation(.bouncy(extraBounce: 0.2)) {
+                    showdelay = true
                 }
             }
         }
@@ -127,24 +97,20 @@ struct WrappedViewFive: View {
 #Preview {
     WrappedViewFive()
 }
-struct Song {
+struct SpendingSummary {
     let title: String
-    let artist: String
-    let imgess: String
+    let amount: Int
+    let percentage: Double
 }
 
-let topSongs: [Song] = [
-    Song(title: "Dheema (LIK)", artist: "Anirudh Ravichander", imgess: "dheema"),
-    Song(title: "Ordinary person (Leo)", artist: " Anirudh Ravichander & Nikhita Gandhi", imgess: "leo"),
-    Song(title: "Arabic Kuthu (Beast)", artist: "Anirudh Ravichander, Jonita Gandhi", imgess: "beast"),
-    Song(title: "Neethane", artist: "Shreya Ghoshal, A.R.Rahman", imgess: "mersal"),
-    Song(title: "Aathi (Kaththi)", artist: "Anirudh Ravichander, Vishal Dadlani", imgess: "kaththi"),
-    Song(title: "Manasilaayo", artist: "Anirudh Ravichander", imgess: "vettaiyan")
-
+let spendings: [SpendingSummary] = [
+    .init(title: "User", amount: 15000, percentage: 0.492),
+    .init(title: "Airtime", amount: 15000, percentage: 0.419),
+    .init(title: "Merchant", amount: 2400, percentage: 0.890)
 ]
+
 struct AnimatedBackground: View {
     @State private var animate = false
-
     var body: some View {
         ZStack {
             ForEach(0..<5) { i in
@@ -156,10 +122,10 @@ struct AnimatedBackground: View {
                             endPoint: .bottomTrailing
                         )
                     )
-                    .frame(width: CGFloat(100 + (i * 50)), height: CGFloat(100 + (i * 50)))
+                    .frame(width: CGFloat(100 + (i * 50)), height: CGFloat(300 + (i * 50)))
                     .offset(x: animate ? 150 : -150, y: animate ? -150 : 150)
                     .opacity(0.3)
-//                    .blur(radius: 20)
+                    .blur(radius: animate ? 8 : 0)
                     .animation(
                         Animation.easeInOut(duration: 4)
                             .repeatForever(autoreverses: true),
