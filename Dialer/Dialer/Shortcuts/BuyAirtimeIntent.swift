c080d1de70f5964c08db6a0bf412c3bcf85cdb34
Cédric Bahirwe <cedricaganzebahirwe@gmail.com>
2025-01-09 01:03:10 +0200

    add phrases

--- Dialer/Dialer/Shortcuts/BuyAirtimeIntent.swift
+++ Dialer/Dialer/Shortcuts/BuyAirtimeIntent.swift
@@ -9,19 +9,18 @@
 import Foundation
 import AppIntents
 
-//@available(iOS 18.0, *)
+@available(iOS 18.0, *)
 struct BuyAirtimeIntent: AppIntent {
     static let title: LocalizedStringResource = "Buy airtime"
 
     @Parameter(
         title: "airtime amount",
-        controlStyle: .stepper,
-        inclusiveRange: (100, 10_000)
+        inclusiveRange: (100, 10_000),
+        requestValueDialog: .init("How much airtime do you want?")
     )
     var amount: Int
 
     func perform() async throws -> some IntentResult & OpensIntent {
-        print("Amount", amount)
         let airtimTransaction = AirtimeTransaction(amount: amount)
 
         if let telUrl = try? airtimTransaction.getUSSDURL() {
@@ -33,28 +32,49 @@ struct BuyAirtimeIntent: AppIntent {
     }
 }
 
-
+@available(iOS 18.0, *)
 struct DialerShortcuts: AppShortcutsProvider {
     static var appShortcuts: [AppShortcut] {
-        var shortcuts: [AppShortcut] = []
-
-//        if #available(iOS 18.0, *) {
-            print("We went here")
-            let airtimeShortcut = AppShortcut(
-                intent: BuyAirtimeIntent(),
-                phrases: [
-                    "Buy airtime",
-                    "Buy \(.applicationName) airtime",
-                    "Get airtime",
-                    "Buy airtime on \(.applicationName)",
-                    "Buy airtime with \(.applicationName)"
-                ],
-                shortTitle: "Buy airtime",
-                systemImageName: "simcard"
-            )
-            shortcuts.append(airtimeShortcut)
-//        }
-
-        return shortcuts
+        AppShortcut(
+            intent: BuyAirtimeIntent(),
+            phrases: [
+                "Airtime",
+                "Get quick airtime",
+                "Buy \(.applicationName) airtime",
+                "Momo airtime",
+                "Get airtime",
+                "Buy airtime on \(.applicationName)",
+                "Buy airtime with \(.applicationName)",
+                "\(.applicationName) airtime"
+            ],
+            shortTitle: "Buy airtime",
+            systemImageName: "simcard"
+        )
     }
 }
+
+//
+//struct DialerShortcutss: AppShortcutsProvider {
+//    static var appShortcuts: [AppShortcut] {
+//        var shortcuts: [AppShortcut] = []
+//
+////        if #available(iOS 18.0, *) {
+//            print("We went here")
+//            let airtimeShortcut = AppShortcut(
+//                intent: BuyAirtimeIntent(),
+//                phrases: [
+//                    "Buy airtime",
+//                    "Buy \(.applicationName) airtime",
+//                    "Get airtime",
+//                    "Buy airtime on \(.applicationName)",
+//                    "Buy airtime with \(.applicationName)"
+//                ],
+//                shortTitle: "Buy airtime",
+//                systemImageName: "simcard"
+//            )
+//            shortcuts.append(airtimeShortcut)
+////        }
+//
+//        return shortcuts
+//    }
+//}
