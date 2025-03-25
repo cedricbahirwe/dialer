aeccd072f204aa3f1d7c3dbc7961dcd668fbdbe2
Cédric Bahirwe <cedricaganzebahirwe@gmail.com>
2025-03-25 14:24:30 +0200

    Improved CodePin

--- Dialer/Dialer/Models/CodePin.swift
+++ Dialer/Dialer/Models/CodePin.swift
@@ -9,54 +9,36 @@ import Foundation
 
 // https://www.gsmarena.com/glossary.php3?term=pin-code
 struct CodePin: Codable {
-    private let a: Int
-    private let b: Int
-    private let c: Int
-    private let d: Int
-    private let e: Int
-    
+    private let digits: [Int]
+    private static let maxLength: Int = 5
+
+    private init(digits: [Int]) throws {
+        guard digits.count == CodePin.maxLength else {
+            throw ValidationError.invalidCount(digits.count)
+        }
+        guard digits.allSatisfy({ (0...9).contains($0) }) else {
+            throw ValidationError.invalidDigits
+        }
+        self.digits = digits
+    }
+
     init(_ value: String) throws {
-        guard value.allSatisfy({ $0.isWholeNumber }) else {
-            throw ValidationError.invalidCharacters
-        }
-
-        guard value.count == 5 else {
-            throw ValidationError.invalidCount(value.count)
-        }
-
-        // TODO: # This needs improvements
-        let digits = Array(value)
-        self.a = digits[0].wholeNumberValue!
-        self.b = digits[1].wholeNumberValue!
-        self.c = digits[2].wholeNumberValue!
-        self.d = digits[3].wholeNumberValue!
-        self.e = digits[4].wholeNumberValue!
+        let parsedDigits = value.compactMap { $0.wholeNumberValue }
+        try self.init(digits: parsedDigits)
     }
 
     init(_ value: Int) throws {
-        guard String(value).count == 5 else {
-            throw ValidationError.invalidCount(String(value).count)
-        }
-
-        // TODO: # This needs improvements
-        let digits = Array(String(value))
-        self.a = digits[0].wholeNumberValue!
-        self.b = digits[1].wholeNumberValue!
-        self.c = digits[2].wholeNumberValue!
-        self.d = digits[3].wholeNumberValue!
-        self.e = digits[4].wholeNumberValue!
+        let parsedDigits = String(value).compactMap { $0.wholeNumberValue }
+        try self.init(digits: parsedDigits)
     }
 }
 
 extension CodePin {
-    var asString: String { "\(a)\(b)\(c)\(d)\(e)" }
-
-    var asNumber: Int { Int(asString) ?? 0 }
-    
-    var digits: Int { asString.count }
+    var asString: String { digits.map(String.init).joined() }
     
     enum ValidationError: Error {
         case invalidCharacters
         case invalidCount(Int)
+        case invalidDigits
     }
 }
